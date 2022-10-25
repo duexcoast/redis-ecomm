c3a9c378a8c430964e2829a80698113549d66730
duexcoast <conor.ux@gmail.com>
2022-10-25 11:12:27 -0400

    getUserById and deserialize implemented with redis

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -6,7 +6,11 @@ import { set_attributes } from 'svelte/internal';
 
 export const getUserByUsername = async (username: string) => {};
 
-export const getUserById = async (id: string) => {};
+export const getUserById = async (id: string) => {
+	const user = await client.hGetAll(usersKey(id));
+
+	return deserialize(id, user);
+};
 
 export const createUser = async (attrs: CreateUserAttrs) => {
 	const id = genId();
@@ -17,6 +21,14 @@ export const createUser = async (attrs: CreateUserAttrs) => {
 
 const serialize = (user: CreateUserAttrs) => {
 	return {
+		username: user.username,
+		password: user.password
+	};
+};
+
+const deserialize = (id: string, user: { [key: string]: string }) => {
+	return {
+		id,
 		username: user.username,
 		password: user.password
 	};
